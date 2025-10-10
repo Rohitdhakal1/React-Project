5ee9c400e85d8f187bd41b1dbd30a0faea71f616
Rohit dhakal <rohit.22scse1010932@galgotiasuniversity.edu.in>
2025-10-10 20:02:37 +0530

    Add files via upload

---
+++ Meal_APi Fetch(axios)/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Meals from './components/Meals'
+
+const App = () => {
+  return (
+    <div><Meals/></div>
+  )
+}
+
+export default App
---
+++ Meal_APi Fetch(axios)/Meals.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
+import './style.css'
+
+const Meals = () => {
+
+    const[items,setItems] = useState([]);
+
+    useEffect(()=>{
+        axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(res=> {
+           setItems(res.data.meals)
+        }).catch((err)=>{
+            console.error( `api problem ${err}`);
+        });
+
+    },[]) 
+
+    const itemsList = ()=>items.map(({strMeal,strMealThumb,idMeal})=>{
+        return (
+        <section className='card' key={idMeal}> 
+
+         <img src={strMealThumb} alt="mealPhoto"/>
+
+         <section className='content'>
+            <p> {strMeal} </p>
+            <p>#{idMeal}</p>
+         </section>
+
+          </section>
+    )})
+
+  return (
+    <div className='items-container'>
+        {itemsList()}
+    </div>
+  )
+}
+
+export default Meals
